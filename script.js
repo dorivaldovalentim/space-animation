aa18f96b9998649b30dca11e4c7dda562836fd69
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-08-22 15:38:40 -0400

    Desenhando as estrelas e a lua (The Space)

---
+++ script.js
@@ -0,0 +1,24 @@
+let container = document.querySelector('#container'), i = 1;
+
+while(i <= 100) {
+	let item = document.createElement('div');
+	item.classList.add('star');
+
+	let random_size = Math.ceil(Math.random() * 4);
+
+	let random_position = {
+		x: Math.ceil(Math.random() * container.offsetWidth),
+		y: Math.ceil(Math.random() * container.offsetHeight)
+	}
+
+	if (Math.ceil(Math.random() * 10) == i) {
+		item.style.filter = 'invert(1)';
+	}
+
+	item.style.width =  random_size + 'px';
+	item.style.height = item.style.width;
+	item.style.transform = `translateX(${random_position.x}px) translateY(${random_position.y}px)`;
+	container.appendChild(item);
+
+	i++;
+}
